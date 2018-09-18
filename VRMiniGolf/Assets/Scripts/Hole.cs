e6f1b675198bf6ae63f4d747290974cf288b4d84
ryan-chomistek <ryan.chomistek@tamu.edu>
2018-09-17 19:12:02 -0500

    stuff

---
+++ VRMiniGolf/Assets/Scripts/Hole.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Ball ball = other.GetComponent<Ball>();
+        if(ball)
+        {
+            Debug.Log("ball in hole!");
+        }
+    }
+}
